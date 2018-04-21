afb9c485800b6160a6b9864efcb3dc0ba805d7d5
AceForks <740307766@qq.com>
2018-04-21 09:25:26 +0800

    update

---
+++ src/Problem.java
@@ -0,0 +1,17 @@
+public class Problem {
+    public Problem() { }
+
+    public void solve(int problemNum) {
+        switch (problemNum) {
+            case 1:
+                one();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void one() {
+
+    }
+}
