4ad272c7a719a94d246386611e268bece2c02609
AceForks <740307766@qq.com>
2018-09-28 18:08:47 +0800

    solution 9

--- src/Solution.java
+++ src/Solution.java
@@ -211,4 +211,23 @@ public class Solution {
         else
             return (res < (double)Integer.MIN_VALUE)?Integer.MIN_VALUE:(int)res;
     }
+
+    //for problem 9
+    public boolean isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        if(x == 0)
+            return true;
+        int[] num = new int[10];
+        int len = 0;
+        while(x > 0) {
+            num[len++] = x%10;
+            x /= 10;
+        }
+        for(int i = 0; i < len/2; i++) {
+            if(num[i] != num[len-i-1])
+                return false;
+        }
+        return true;
+    }
 }
